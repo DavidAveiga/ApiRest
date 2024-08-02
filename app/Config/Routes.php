098f261bb2cffeac105b2a5cafea2cc412cf9c10
DavidAveiga <DavidAveiga>
2024-08-02 16:48:38 -0500

    modificado

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,3 +7,6 @@ use CodeIgniter\Router\RouteCollection;
  */
 //$routes->get('/', 'Home::index');
 $routes->get('/deber', 'Home::index');
+$routes->get('/insterta', 'Home::inserta');
+$routes->get('/editar', 'Home::editar');
+$routes->get('/eliminar', 'Home::eliminar');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,20 +4,53 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index()
-    {
-   //    $valor =[
-    //   'test'=> 'ejemplo',
-      //  'id' => 1
-   //    ];
+public function index()
+ {
+$this->db= \Config\Database :: connect();
+$query = $this->db->query('SELECT NOMBRES, SEXO, EDAD, PESO FROM `datos_usuario`.`datos`limit 10;');
+$valor=($query->getResult());
+var_dump($valor);
+return $this-> response-> setJSON ($valor);
+ }
 
-     //  return$this ->response ->setJSON($valor);
-    $this->db= \Config\Database :: connect();
-    $query = $this->db->query('SELECT NOMBRES, SEXO, EDAD, PESO FROM `datos_usuario`.`datos`limit 10;');
-   
-    $valor=($query->getResult());
-    var_dump($valor);
-    return $this-> response-> setJSON ($valor);
-       //return view('welcome_message');
-    }
+
+public function inserta($NombreN,$CedulaN,$CiudadN,$TelefonoN)
+{
+$this->db= \Config\Database :: connect();
+$query = $this->db->query("INSERT INTO `datos_usuario`.`nuevo_dato`
+ (NombreN, CedulaN, CiudadN, TelefonoN)
+VALUES
+(<{$NombreN: }>,
+<{$CedulaN: }>,
+<{$CiudadN: }>,
+<{$TelefonoN: }>);
+SELECT * FROM datos_usuario.nuevo_dato;");
+$valor=($query->getResult());
+return $this-> response-> setJSON ($valor);
+}
+
+
+public function editar($NombreN,$CedulaN,$CiudadN,$TelefonoN)
+{
+$this->db= \Config\Database :: connect();
+$query = $this->db->query("UPDATE `datos_usuario`.`nuevo_dato`
+SET`NombreN` = <{$NombreN: }>,`CedulaN` = <{$CedulaN: }>,`CiudadN` = <{$CiudadN: }>,
+`TelefonoN` = <{$TelefonoN: }>
+WHERE `$NombreN` = <{expr}>;");
+$valor=($query->getResult());
+return $this-> response-> setJSON ($valor);
+}
+
+
+public function eliminar($TelefonoN)
+{
+$this->db= \Config\Database :: connect();
+$query = $this->db->query
+try {
+   $builder = $this->db->table(`nuevo_dato`);
+   $builder->where(`TelefonoN`,$TelefonoN);
+   $builder->delete();
+$valor=($query->getResult());
+return $this-> response-> setJSON ($valor);
+}
 }
